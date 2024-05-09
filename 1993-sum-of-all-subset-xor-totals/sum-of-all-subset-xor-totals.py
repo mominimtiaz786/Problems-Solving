0284feb67b15187476bf136092413aa4aa5249d2
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2024-05-09 23:17:23 +0500

    Time: 48 ms (66.27%) | Memory: 16.4 MB (84.77%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         N = len(nums)
-        allXORs = []
+        xorSum = 0
 
         def backTrack(XOR,index):
+            nonlocal xorSum 
             if index == N:  return
 
             for i in range(index,N):
                 curr = XOR ^ nums[i]
-                allXORs.append(curr)
+                xorSum+=curr
                 backTrack(curr, i+1)
-                
+
         backTrack(0,0)
-        return sum(allXORs)
+        return xorSum
