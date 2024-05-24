5231f29d4213ac8133d8d6af74e08280598c5c88
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2024-05-24 16:57:40 +0500

    Time: 281 ms (8.39%) | Memory: 16.5 MB (99.55%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
@@ -11,47 +11,80 @@ class Solution:
 
     #     return True
     
-    def checkIsValid(self, words, lettersCount):
-        wordsLetters = []
-        for word in words:
-            wordsLetters= wordsLetters + [ch for ch in word]
-        wordsLetterCounter = Counter(wordsLetters)
+
+    # def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+    #     lettersCount = Counter(letters)
+    #     letters = set(letters)
+    #     N = len(words)
+
+    #     lettersScoreDict = {
+    #         chr(i + ord('a')): score[i] if chr(i + ord('a')) in letters else -float('inf')
+    #         for i in range(26)
+    #     }
+
+    #     wordScores = [
+    #         sum([lettersScoreDict[ch] for ch in word]) 
+    #         for word in words
+    #         ]
+
+
+    #     maxSum = 0
+    #     for i in range(2**N):
+    #         wordsComb = []
+    #         combSum = 0
+    #         x = i
+    #         while x:
+    #             msb = math.floor(math.log2(x))
+    #             wordsComb.append(words[msb])
+    #             combSum+=wordScores[msb]
+    #             x^=(1<<msb)
+            
+    #         if combSum > 0 and self.checkIsValid(wordsComb, lettersCount):
+    #             maxSum = max(maxSum, combSum)
         
+    #     return maxSum
+
+    def combSum(self, words, lettersCount, score):
+        combSum = 0 
+        wordsLetterCounter = dict()
+        for word in words:
+            for ch in word:
+                wordsLetterCounter[ch] = wordsLetterCounter.get(ch,0) + 1
 
         for letter in wordsLetterCounter:
             if wordsLetterCounter[letter] > lettersCount.get(letter, 0):
-                return False
+                return 0
+            else:
+                combSum+=(wordsLetterCounter[letter]*score[(ord(letter)- ord('a'))])
 
-        return True
+        return combSum
+    
 
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
         lettersCount = Counter(letters)
-        letters = set(letters)
+        # letters = set(letters)
         N = len(words)
 
-        lettersScoreDict = {
-            chr(i + ord('a')): score[i] if chr(i + ord('a')) in letters else -float('inf')
-            for i in range(26)
-        }
+        # lettersScoreDict = {
+        #     chr(i + ord('a')): score[i] if chr(i + ord('a')) in letters else -float('inf')
+        #     for i in range(26)
+        # }
 
-        wordScores = [
-            sum([lettersScoreDict[ch] for ch in word]) 
-            for word in words
-            ]
+        # wordScores = [
+        #     sum([lettersScoreDict[ch] for ch in word]) 
+        #     for word in words
+        #     ]
 
 
         maxSum = 0
         for i in range(2**N):
             wordsComb = []
-            combSum = 0
             x = i
             while x:
                 msb = math.floor(math.log2(x))
                 wordsComb.append(words[msb])
-                combSum+=wordScores[msb]
                 x^=(1<<msb)
             
-            if combSum > 0 and self.checkIsValid(wordsComb, lettersCount):
-                maxSum = max(maxSum, combSum)
+            maxSum = max(maxSum, self.combSum(wordsComb, lettersCount, score))
         
         return maxSum
