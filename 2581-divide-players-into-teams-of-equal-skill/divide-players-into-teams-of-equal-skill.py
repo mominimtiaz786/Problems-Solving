f6c1357f9af09e76d5af67582e96cf9994e91590
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2024-10-07 14:08:14 +0300

    Time: 417 ms (27.89%) | Memory: 29.5 MB (41.34%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -5,14 +5,14 @@ class Solution:
 
         skillsum = skill[0] + skill[-1]
         l,r = 0, N-1
-        teams = []
+        chemistry=0
         while l<r:
             if skill[l] + skill[r] == skillsum:
-                teams.append((skill[l],skill[r]))
+                chemistry+=(skill[l]*skill[r])
                 l+=1
                 r-=1
             else:
                 return -1
         
 
-        return sum([t[0]*t[1] for t in teams])
+        return chemistry
