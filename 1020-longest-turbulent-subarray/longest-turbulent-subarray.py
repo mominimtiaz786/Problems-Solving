5fb336d14a2a615a3166cf28542f0cc288acfb3a
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-11-29 19:11:38 +0500

    Time: 31 ms (97.40%) | Memory: 20.5 MB (73.17%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
@@ -1,31 +1,29 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
         N = len(arr)
-        if N < 2:   return N
+        if N == 1:   return N
 
         max_size = 1
+        curr_size = 1
 
-        curr_size1, curr_size2 = 1, 1
+        smaller = True
+
 
         for i in range(N-1):
 
-            if i % 2 == 0 and arr[i] < arr[i+1]:
-                curr_size1+=1
-            elif i % 2 == 1 and arr[i] > arr[i+1]:
-                curr_size1+=1
+            if arr[i] < arr[i+1] and smaller:
+                curr_size+=1
+                smaller = not smaller
+            
+            elif arr[i] > arr[i+1] and not smaller:
+                curr_size+=1
+                smaller = not smaller
+            
             else:
-                max_size = max(max_size, curr_size1)
-                curr_size1 = 1
-
-            j = i+1          
-            if j % 2 == 1 and arr[j] < arr[j-1]:
-                curr_size2+=1
-            elif j % 2 == 0 and arr[j] > arr[j-1]:
-                curr_size2+=1
-            else:
-                max_size = max(max_size, curr_size2)
-                curr_size2 = 1  
+                max_size = max(max_size, curr_size)
+                curr_size = 2
+                if arr[i] == arr[i+1]:
+                    curr_size = 1
 
 
-
-        return max(max_size, curr_size1, curr_size2)
+        return max(max_size, curr_size)
