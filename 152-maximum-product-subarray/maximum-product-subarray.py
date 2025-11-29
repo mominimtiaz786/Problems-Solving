6783488d6e68aee05a30f019e101600129c48ea0
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-11-30 00:36:29 +0500

    Time: 11 ms (52.77%) | Memory: 18.5 MB (17.53%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,15 +5,9 @@ class Solution:
         result = max_p = min_p = nums[0]
 
         for i in range(1,N):
-            # if max_p < min_p:
-            #     min_p, max_p = max_p, min_p
-            # if nums[i] < 0:
-                # min_p, max_p = max_p, min_p
 
-            c_max = max(nums[i], nums[i]*max_p, nums[i]*min_p)
-            c_min = min(nums[i], nums[i]*min_p, nums[i]*max_p)
-
-            max_p, min_p = c_max, c_min
+            nums_to_compare = [nums[i], nums[i]*max_p, nums[i]*min_p]
+            max_p, min_p = max(nums_to_compare), min(nums_to_compare)
 
             result = max(result, max_p, min_p)
         
