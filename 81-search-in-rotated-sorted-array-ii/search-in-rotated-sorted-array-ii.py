0fea37de7070ebdb28fb7de871ee85938092098d
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-01 06:32:13 +0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (78.97%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,25 +1,28 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        # N = len(nums)
-        # l, r = 0, N-1
+    def search(self, nums: List[int], target: int) -> bool:
+        N = len(nums)
+        l,r =0, N-1
 
-        # while l<=r:
-        #     mid = (l+r)//2
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid]==target:   return True
 
-        #     if target == nums[mid]: return True
+            if nums[l]<nums[mid]:
 
-        #     if nums[l] <= nums[mid]:
-        #         if nums[l]<=target<nums[mid]:
-        #             r=mid-1
-        #         else:
-        #             l = mid+1
+                if nums[l]<=target<nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
 
-        #     else:
-        #         if nums[mid] < target <= nums[r]:
-        #             l = mid+1
-        #         else:
-        #             r = mid-1
-        
-        # return False
+            elif nums[l]>nums[mid]:
 
-        return target in set(nums)
+                if nums[mid]<target<=nums[r]:
+                    l=mid+1
+                else:
+                    r=mid-1
+
+
+            else:
+                l+=1
+
+        return False
