6c40cfb7df76681ff99e66ed321b4941d9276666
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-03 23:49:08 +0500

    Time: 270 ms (10.89%) | Memory: 20.3 MB (19.03%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def findDivisor(self, n1, n2):
+        n1, n2 = (n1, n2) if n1 < n2 else (n2, n1)
+        divisor = 1
+        for i in range(n1, 1, -1):
+            if n1 % i == 0 and n2 % i == 0:
+                divisor=i
+                break
+
+        return divisor
+
+
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:    return head
+        if not head.next:   return head
+
+        curr, nex = head, head.next
+
+        while nex:
+            divisor = self.findDivisor(curr.val, nex.val)
+
+            tmp = ListNode(val=divisor, next=nex)
+            curr.next = tmp
+
+            curr = nex
+            nex = nex.next
+        
+
+        return head
