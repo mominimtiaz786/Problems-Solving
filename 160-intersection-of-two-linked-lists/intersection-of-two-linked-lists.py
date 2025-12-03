683df955254db698a65e48f976658c1cdcff6d3e
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-04 01:06:06 +0500

    Time: 94 ms (12.26%) | Memory: 27.8 MB (73.99%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,28 +6,28 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        pA = ListNode(0)
-        pA.next = headA
-        pB = ListNode(0)
-        pB.next = headB
+        pA = headA
+        pB = headB
+        while pA != pB:
+            if pA:  pA = pA.next
+            else:   pA = headB
 
-        # while pA != pB:
-        #     if pA:  pA = pA.next
-        #     else:   pA = headB
+            if pB:  pB = pB.next
+            else:   pB = headA
+        
+        return pA
 
-        #     if pB:  pB = pB.next
-        #     else:   pB = headA
-        
-        # return pA
+        # pA, pB = ListNode(0), ListNode(0)
+        # pA.next, pB.next = headA, headB
 
-        my_set = set()
-        while pA.next:
-            my_set.add(pA.next)
-            pA = pA.next
+        # my_set = set()
+        # while pA.next:
+        #     my_set.add(pA.next)
+        #     pA = pA.next
         
-        while pB.next:
-            if pB.next in my_set:
-                return pB.next
-            pB = pB.next
+        # while pB.next:
+        #     if pB.next in my_set:
+        #         return pB.next
+        #     pB = pB.next
         
-        return None
+        # return None
