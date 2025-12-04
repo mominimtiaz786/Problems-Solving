b06aae8922c4457b4d2dee51a314611b2f47a15e
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-04 20:41:21 +0500

    Time: 4 ms (15.07%) | Memory: 17.9 MB (17.18%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        my_set = set()
+        MAX_N = 2**31 
+
+        while n not in my_set and n<MAX_N:
+            if n == 1:  return True
+            my_set.add(n)
+
+            x = n
+            n = 0
+            while x:
+                n+=((x%10)**2)
+                x = x//10
+
+
+        return False
+
+        
