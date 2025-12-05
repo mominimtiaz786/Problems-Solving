8527fd70bf14575bd9f1710986a43467d7125ba4
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-06 01:17:23 +0500

    Time: 249 ms (36.93%) | Memory: 17.7 MB (94.92%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        N = len(answerKey)
+        if N <= 2:  return N
+
+        max_count = 0
+
+        max_count = 0
+        for elem in ['F','T']:
+            i = 0
+            wrong = 0   
+
+            for j, ch in enumerate(answerKey):
+                if ch != elem:
+                    wrong += 1
+
+                while wrong > k:
+                    if answerKey[i] != elem:
+                        wrong -= 1
+                    i += 1
+
+                max_count = max(max_count, j - i + 1)            
+
+        return max_count
