b1351bf003bd2c9df90f94ac9233219372b1507e
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-06 05:36:49 +0500

    Time: 1539 ms (5.21%) | Memory: 48.2 MB (15.62%) - LeetSync

--- 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.py
+++ 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.py
@@ -2,17 +2,18 @@ class Solution:
     def countWays(self, ranges: List[List[int]]) -> int:
         ranges.sort(key= lambda x: (x[0], x[1]))
 
-        non_overlapping_ranges = [ranges.pop(0)]
+        last_range = ranges.pop(0)
+        count = 1
 
         while ranges:
             range_to_append = ranges.pop(0)
 
-            if non_overlapping_ranges[-1][1] >= range_to_append[0]:
-                non_overlapping_ranges[-1][1] = max(range_to_append[1], non_overlapping_ranges[-1][1])
+            if last_range[1] >= range_to_append[0]:
+                last_range[1] = max(range_to_append[1], last_range[1])
             else:
-                non_overlapping_ranges.append(range_to_append)
+                last_range = range_to_append
+                count+=1
         
-        count = len(non_overlapping_ranges)
 
         return (2**count) % (10**9 + 7)
 
