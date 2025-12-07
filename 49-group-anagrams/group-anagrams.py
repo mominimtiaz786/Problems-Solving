87594b3e5ce8da8efd924f2324883d76dc331e9d
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-07 19:44:35 +0500

    Time: 53 ms (5.05%) | Memory: 25.2 MB (11.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        hashmap = {}
+        hashmap = defaultdict(list)
 
         for s in strs:
             freq_map = [0]*26
@@ -9,9 +9,6 @@ class Solution:
                 freq_map[ord(k)-ord('a')]+=1
             
             key = tuple([(i,n) for i,n in enumerate(freq_map) if n])
-            if key not in hashmap:
-                hashmap[key]=[]
-
             hashmap[key].append(s)
 
         return list(hashmap.values())
