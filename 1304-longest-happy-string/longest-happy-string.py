af27eec69a91fe2f6aae027b73f8129ae7b93524
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-07 17:43:51 +0500

    Time: 3 ms (15.33%) | Memory: 17.7 MB (91.89%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        
+        counter = {'a': a, 'b': b, 'c': c}
+        
+        req_string = ""
+        last_two = ""
+
+        for i in range(a+b+c):
+
+            valid_choices = [
+                ch for ch in 'abc' if counter[ch]
+                if counter[ch] and 2*ch != last_two
+                ]
+
+
+            if not valid_choices:   break
+
+            choice = sorted(valid_choices,
+                key = lambda x: (counter[x], x),
+                reverse = True
+            )[0]
+            
+            req_string+=choice
+            counter[choice]-=1
+            last_two = last_two[-1]+ choice if len(last_two) == 2 else last_two+ choice
+
+        return req_string
