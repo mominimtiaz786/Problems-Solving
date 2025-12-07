5fbcfa86f19fca05696affdf1cf9abc90f1f1fd7
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-07 19:45:34 +0500

    Time: 11 ms (83.06%) | Memory: 20.8 MB (54.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,11 +4,7 @@ class Solution:
         hashmap = defaultdict(list)
 
         for s in strs:
-            freq_map = [0]*26
-            for k in s: 
-                freq_map[ord(k)-ord('a')]+=1
-            
-            key = tuple([(i,n) for i,n in enumerate(freq_map) if n])
+            key = "".join(sorted(s[:]))
             hashmap[key].append(s)
 
         return list(hashmap.values())
