d1e13a18e0c7201845642dbed0b3ef4c701778e7
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-08 23:34:17 +0500

    Time: 19 ms (37.00%) | Memory: 20.9 MB (54.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,41 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
+    # hash map
+    # nat tan
+    # ant -> [tan, nat]
 
-        hashmap = defaultdict(list)
+    # groups 
 
-        for s in strs:
-            key = "".join(sorted(s[:]))
-            hashmap[key].append(s)
+    # strs = ["eat","tea","tan","ate","nat","bat"]
+    
+    # aet - grp[0]
+    # eat, tea -> key 0
+    # tan -> ant -> key ant
+    # ate -> aet -> key tea
 
-        return list(hashmap.values())
+        groups = {}
+
+        # eat, tea, ate
+        # [a,b,c...], [0101], 
+        # one loop for computing arrays
+        # one loop for [0, 1, 0], [0, 1, 0], [1, 0, 0], [1] 
+
+        for i in strs: # number of words
+            # print("word: ", strs[i])
+            key = ''.join(sorted(i)) #sorting length of string -> k log k
+            # check if key exists in group
+            if key not in groups:
+                print("key is in group")
+                groups[key] = []
+            print("key:", key)
+            groups[key].append(i)
+        
+        return list(groups.values())
+
+        # 
 
         
+
+        
+
