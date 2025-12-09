f744d5354f93d3a389746a38d2e9db7b26fdf7c6
Momin Imtiaz <59701170+mominimtiaz786@users.noreply.github.com>
2025-12-09 15:52:42 +0500

    Time: 35 ms (79.26%) | Memory: 18.3 MB (80.46%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        NP = len(p)
+        NS = len(s)
+        if NS < NP:   return []
+        ans = []
+
+        sliding_freq = {ch:0 for ch in s}
+        p_freq = {ch:0 for ch in s+p}
+        for ch in p:    p_freq[ch]+=1
+
+
+        for j in range(NS):
+            sliding_freq[s[j]]+=1
+
+            if j>= NP:
+                sliding_freq[s[j-NP]]-=1
+                
+            if sliding_freq == p_freq:
+                ans.append(j-NP+1)
+
+        return ans
+
+
+        
